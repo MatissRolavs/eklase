ae72bfbbaf9223b6efc6cd07f4094fb914147118
matissbby <matissrolavs2005@gmail.com>
2025-06-05 11:17:59 +0300

    dwadawd

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\StudentsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
